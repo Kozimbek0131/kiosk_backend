87fa7819f9a17c1e95f807b49f382b6cc26e7ac0
Kozimbek <axatovkozimbek@gmail.com>
2026-03-05 15:13:45 +0500

    Admin panelga Import/Export funksiyasi qo'shildi

--- api/admin.py
+++ api/admin.py
@@ -1,14 +1,29 @@
 from django.contrib import admin
+from import_export import resources, fields
+from import_export.widgets import ForeignKeyWidget
 from import_export.admin import ImportExportModelAdmin
 from .models import Employee, Department
 
+# Bo'limlarni nomiga qarab avtomatik bog'lash uchun resurs
+class EmployeeResource(resources.ModelResource):
+    department = fields.Field(
+        column_name='department',
+        attribute='department',
+        widget=ForeignKeyWidget(Department, 'name_uz') # Excelda bo'lim nomini yozsangiz ham bo'ladi
+    )
+
+    class Meta:
+        model = Employee
+        # Exceldagi ustunlar tartibi
+        fields = ('id', 'full_name_uz', 'full_name_ru', 'full_name_en', 
+                  'position_uz', 'position_ru', 'position_en', 
+                  'department', 'floor', 'room', 'phone')
+
 @admin.register(Employee)
 class EmployeeAdmin(ImportExportModelAdmin):
-    # Ro'yxatda ko'rinadigan ustunlar
-    list_display = ('full_name_uz', 'department', 'floor', 'room', 'phone')
-    # Qidiruv maydonlari
-    search_fields = ('full_name_uz', 'full_name_ru', 'full_name_en')
-    # Filtrlash imkoniyati
+    resource_class = EmployeeResource
+    list_display = ('full_name_uz', 'department', 'floor', 'room')
+    search_fields = ('full_name_uz',)
     list_filter = ('department', 'floor')
 
 @admin.register(Department)
