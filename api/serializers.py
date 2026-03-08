b831e06c74cc648557c79095c393e22b16436874
Kozimbek <axatovkozimbek@gmail.com>
2026-03-08 20:30:22 +0500

    Added openpyxl for xlsx import support

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,34 @@
 from rest_framework import serializers
-from .models import Employee, Department
+from .models import Employee, Department, Leadership
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    # Bo'lim ID raqami o'rniga nomini ko'rsatish
+    department = serializers.ReadOnlyField(source='department.name_uz')
+    image_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Employee
+        fields = [
+            'id', 'full_name_uz', 'full_name_ru', 'full_name_en', 
+            'position_uz', 'position_ru', 'position_en', 
+            'department', 'floor', 'room', 'phone', 'image_url'
+        ]
+
+    def get_image_url(self, obj):
+        if obj.image:
+            request = self.context.get('request')
+            return request.build_absolute_uri(obj.image.url) if request else obj.image.url
+        return None
+
+class LeadershipSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Leadership
         fields = '__all__'
+
+    def get_image_url(self, obj):
+        if obj.image:
+            request = self.context.get('request')
+            return request.build_absolute_uri(obj.image.url) if request else obj.image.url
+        return None
