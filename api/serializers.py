7e838ef7500a884a3166643c21ae36f6d64315dc
Kozimbek <axatovkozimbek@gmail.com>
2026-03-08 19:58:35 +0500

    Added openpyxl for xlsx import support

--- api/serializers.py
+++ api/serializers.py
@@ -1,31 +1,18 @@
-from rest_framework import serializers
-from .models import Employee, Department, Leadership
-
-class DepartmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Department
-        fields = '__all__'
-
 class EmployeeSerializer(serializers.ModelSerializer):
-    department_name = serializers.ReadOnlyField(source='department.name_uz')
+    # 1. Bo'lim nomini ID raqami emas, matn ko'rinishida chiqaradi
+    department = serializers.ReadOnlyField(source='department.name_uz')
+    
+    # 2. Rasmning to'liq manzili (URL) hosil bo'lishini ta'minlaydi
     image_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Employee
-        fields = '__all__'
-
-    def get_image_url(self, obj):
-        if obj.image:
-            request = self.context.get('request')
-            return request.build_absolute_uri(obj.image.url) if request else obj.image.url
-        return None
-
-class LeadershipSerializer(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Leadership
-        fields = '__all__'
+        # 3. 'phone' maydoni JSON'da chiqishi uchun uni aniq ko'rsatib qo'yamiz
+        fields = [
+            'id', 'full_name_uz', 'full_name_ru', 'full_name_en', 
+            'position_uz', 'position_ru', 'position_en', 
+            'department', 'floor', 'room', 'phone', 'image_url'
+        ]
 
     def get_image_url(self, obj):
         if obj.image:
