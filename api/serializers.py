2fcc84a2bdb696ddaea21e467b4da402674ce206
Kozimbek <axatovkozimbek@gmail.com>
2026-03-08 20:06:25 +0500

    Added openpyxl for xlsx import support

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 class EmployeeSerializer(serializers.ModelSerializer):
-    # ID raqami o'rniga bo'lim nomini matn ko'rinishida chiqarish
+    # ID raqami (5, 6) o'rniga bo'lim nomini matn ko'rinishida chiqarish
     department = serializers.ReadOnlyField(source='department.name_uz')
     
     # Telefon raqami bo'sh bo'lishiga ruxsat beramiz
@@ -9,7 +9,7 @@ class EmployeeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Employee
-        # Hamma maydonlar ro'yxatda borligini tekshiring
+        # Barcha maydonlar ro'yxatda borligini tekshiring
         fields = [
             'id', 'full_name_uz', 'full_name_ru', 'full_name_en', 
             'position_uz', 'position_ru', 'position_en', 
