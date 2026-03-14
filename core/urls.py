385a138fbff04316d5b046ee639057ca9eca779e
Kozimbek <axatovkozimbek@gmail.com>
2026-03-14 09:51:55 +0500

    Fixed re_path error in urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,21 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path # re_path to'g'ri import qilindi
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve # Bu qator rasmlarni xavfsiz ochish uchun kerak
-import re
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')), 
+    path('api/', include('api.urls')),
 ]
 
-# Rasmlar va statik fayllarni istalgan rejimda (Debug True yoki False) ishlashi uchun:
+# Media va Static fayllar uchun xavfsiz yo'llar
 urlpatterns += [
-    re.path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re.path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-# Qo'shimcha xavfsizlik uchun standart usulni ham qoldiramiz
+# Qo'shimcha ravishda standart static ulanishi
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
